2af34746b6ba1ec948dbd0fc913f8d6da67e8508
Vovk Viktor <viktor.vovk@vilmate.com>
2020-03-12 13:23:34 +0200

    hot fix

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -7,9 +7,9 @@ const initialState = {
 export const count = (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT_COUNT:
-      return { count: state.count++ };
+      return { ...state, count: state.count++ };
     case DECREMENT_COUNT:
-      return { count: state.count-- };
+      return { ...state, count: state.count-- };
 
     default:
       break;
